ca668a810333ef6e1e5a364b1a78c7fb933b86d8
Austin-William Lo <austin-william.lo@epitech.eu>
2021-12-25 13:13:21 +0100

    [ADD] welcome text

--- src/pages/Cv.js
+++ src/pages/Cv.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "../styles/Cv.css";
 
-function CV() {
+function Cv() {
     return (
-        <div className="CV">
-            <h1>CV</h1>
+        <div className="Cv" id="home">
+            <div className="Cv-container">
+                <div className="Cv-title">
+                    <p>Bienvenue</p>
+                    <b>
+                        <div class="Cv-animated-text">
+                            dans mon CV en ligne<br />
+                            pour voir qui je suis<br />
+                            ce que j'ai fais<br />
+                            ce que j'ai appris<br />
+                            bonne lecture !
+                        </div>
+                    </b>
+                </div>
+            </div>
         </div>
     );
 }
 
-export default CV;
+export default Cv;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import CV from './Cv';
+import Cv from './Cv';
 import "../styles/Home.css";
 
 function Home() {
     return (
         <div className="Home">
             <div className="Home-container">
-                <CV />
+                <Cv />
             </div>
         </div>
     );
