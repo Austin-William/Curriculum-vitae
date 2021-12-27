8ea5a48a3e724c7238c34ed39d537a69c5488a9e
Austin-William Lo <austin-william.lo@epitech.eu>
2021-12-27 11:12:11 +0100

    [ADD] ui hobbies

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -8,26 +8,28 @@ function Hobbies() {
 
     return (
         <div className="Hobbies" id="hobbies">
-            <h2>
-                {t("cv.hobbies.title")}
-            </h2>
-            <hr />
-            <div className="Hobbies-description">
-                <p>
-                    {t("cv.hobbies.hobby-1")}
-                </p>
-                <p>
-                    {t("cv.hobbies.hobby-2")}
-                </p>
-                <p>
-                    {t("cv.hobbies.hobby-3")}
-                </p>
-                <p>
-                    {t("cv.hobbies.hobby-4")}
-                </p>
-                <p>
-                    {t("cv.hobbies.hobby-5")}
-                </p>
+            <div className="Hobbies-container">
+                <h2>
+                    {t("cv.hobbies.title")}
+                </h2>
+                <hr />
+                <div className="Hobbies-description">
+                    <p>
+                        <i className='fas fa-basketball-ball' /> {t("cv.hobbies.hobby-1")}
+                    </p>
+                    <p>
+                        <i className='fas fa-gamepad' /> {t("cv.hobbies.hobby-2")}
+                    </p>
+                    <p>
+                        <i className='fas fa-film' /> {t("cv.hobbies.hobby-3")}
+                    </p>
+                    <p>
+                        <i className='fas fa-edit' /> {t("cv.hobbies.hobby-4")}
+                    </p>
+                    <p>
+                        <i className='fas fa-wind' /> {t("cv.hobbies.hobby-5")}
+                    </p>
+                </div>
             </div>
         </div>
     );
