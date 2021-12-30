11f3c67a5846b686adb3686282e40be47ff24eb9
Austin-William Lo <austin-william.lo@epitech.eu>
2021-12-30 15:32:14 +0100

    [ADD] ui animated background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
+import "./styles/App.css";
 
 function App() {
     return (
-        <div className="App">
+        <div className="App" id="home">
             <Header />
             <Home />
         </div>
