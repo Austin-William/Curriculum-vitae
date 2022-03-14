5f59383961f04b6e98235f7e814ebd177004d1a2
Austin-William Lo <austin-william.lo@epitech.eu>
2022-03-14 23:29:11 +0100

    [ADD] header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    NavbarComponent,
+    ProfilComponent,
+    ExperiencesComponent,
+    SkillsComponent,
+    ActivitiesComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
