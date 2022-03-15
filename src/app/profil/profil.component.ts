1ea5fbb6881fc53ef6ff54951821079f389ab5fa
Austin-William Lo <austin-william.lo@epitech.eu>
2022-03-15 12:10:03 +0100

    [ADD] profile

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.css']
 })
-export class ProfilComponent implements OnInit {
-
+export class ProfilComponent {
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  description_1 = `Austin-William Lo, étudiant en 4ème année à Epitech pour devenir Expert en Technologie de l'Information après un BAC +5.`
+  description_2 = `J’ai appris divers langages de programmations tels que le C, C++, Python, Flutter (Dart) ou le Javascript en passant par de l'Haskell par des petits et gros projets afin d'être prêt à travailler en entreprise. Que ce soit pour des projets personnels ou professionnels, j'ai appris à être autonome, à travailler en équipe et à m'adapter à n'importe quelle situation.`;
+
+  profiles = [
+    {
+      status: '✔',
+      description: 'Web'
+    },
+    {
+      status: '✔',
+      description: 'Mobile'
+    },
+    {
+      status: '✔',
+      description: 'Open minded'
+    },
+  ]
 }
