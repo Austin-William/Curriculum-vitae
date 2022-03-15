0786292f08a4a7c202ef647492e4940df13d6a1c
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2022-03-15 14:04:50 +0100

    [UPD] profile

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -11,8 +11,13 @@ export class ProfilComponent {
   ngOnInit() {
   }
 
-  description_1 = `Austin-William Lo, étudiant en 4ème année à Epitech pour devenir Expert en Technologie de l'Information après un BAC +5.`
-  description_2 = `J’ai appris divers langages de programmations tels que le C, C++, Python, Flutter (Dart) ou le Javascript en passant par de l'Haskell par des petits et gros projets afin d'être prêt à travailler en entreprise. Que ce soit pour des projets personnels ou professionnels, j'ai appris à être autonome, à travailler en équipe et à m'adapter à n'importe quelle situation.`;
+  about = `About`;
+  me = `me`;
+
+  description_1 = `Austin-William Lo, 4th year student at Epitech to become an Expert in Information Technology after a BAC +5.`
+  description_2 = `I learned various programming languages ​​such as C, C++, Python, Flutter (Dart) or Javascript through Haskell through small and large projects in order to be ready to work in a company. Whether for personal or professional projects, I learned to be independent, to work in a team and to adapt to any situation.`;
+  description_3 = `i'm searching for a job in the web development field.`;
+  ready_text = `Ready to work !`;
 
   profiles = [
     {
