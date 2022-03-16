1384e858f1eb0099ca12b921869bda3dbf4f8e0f
Austin-William Lo <55129304+Austin-William@users.noreply.github.com>
2022-03-16 20:26:00 +0100

    [ADD] carousel projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,4 +12,34 @@ export class ProjectsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  projects = [
+    {
+      name: 'Redditech',
+      description: 'A mobile application using Flutter and Reddit API to clone Reddit app.',
+      images: [
+        {
+          url: 'assets/projects/redditech/1.png',
+          alt: 'Redditech',
+        },
+        {
+          url: 'assets/projects/redditech/2.png',
+          alt: 'Redditech',
+        },
+        {
+          url: 'assets/projects/redditech/3.png',
+          alt: 'Redditech',
+        },
+        {
+          url: 'assets/projects/redditech/4.png',
+          alt: 'Redditech',
+        },
+        {
+          url: 'assets/projects/redditech/5.png',
+          alt: 'Redditech',
+        },
+      ],
+      link: 'https://github.com/Austin-William/Redditech',
+    }
+  ];
+
 }
